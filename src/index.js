bcde12a79121157c6ddef4b6a2df9bd2f1f1fa44
iamEvgen <evgen.alexa@gmail.com>
2022-04-28 16:45:23 +0300

    set project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const startPage = (function() {
+    const content = document.querySelector('#content');
+
+    const container = document.createElement('div');
+    container.classList.add('container')
+    content.appendChild(container);
+
+})();
+
